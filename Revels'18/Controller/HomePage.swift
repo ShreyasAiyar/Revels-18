124988d289e5fd8b20ba85468e63a99a1ceef56e
ShreyasAiyar <shreyas.aiyar96@gmail.com>
2017-12-08 22:24:54 +0530

    First Version Of Home Page V 1.0

--- Revels'18/Controller/HomePage.swift
+++ Revels'18/Controller/HomePage.swift
@@ -8,17 +8,62 @@
 
 import UIKit
 
-class HomePage: UIViewController{
+class HomePage: UITableViewController {
     
+    let sectionHeaders:[String] = ["Events","Schedule","Results"]
+    @IBOutlet weak var navigationBar: UINavigationItem!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        let imageFrame:CGRect = CGRect(x: 0, y: (self.navigationController?.navigationBar.frame.height)!+20, width:
+            (self.navigationController?.navigationBar.frame.width)!, height: 100)
+        let headerView:UIView = UIView(frame: imageFrame)
+
+        
+        let revelsImage:UIImage! = UIImage(named: "Revels Banner")
+        let image:UIImageView = UIImageView(image: revelsImage!)
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        headerView.addSubview(image)
+        
+        
+        self.tableView.tableHeaderView = headerView
         
 
-        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionHeaders[section]
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeViewCell
+        cell.firstLabel.text = "Conclave"
+        cell.secondLabel.text = "Conclave"
+        cell.thirdLabel.text = "Conclave"
+        cell.fourthLabel.text = "Conclave"
+        return cell
+
+
+    }
     
     
 
---
+++ Revels'18/View/HomeViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewCell.swift
+//  Revels-18
+//
+//  Created by Shreyas Aiyar on 08/12/17.
+//  Copyright © 2017 Shreyas Aiyar. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+    
+
+
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var fourthLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    
+    
+
+
+}
