a020d10d6e5980abb291736da0dc87dbea814222
ShreyasAiyar <shreyas.aiyar96@gmail.com>
2017-12-08 10:32:26 +0530

    Updated UI

---
+++ Revels'18/Controller/HomePage.swift
@@ -0,0 +1,25 @@
+//
+//  HomePage.swift
+//  Revels-18
+//
+//  Created by Shreyas Aiyar on 08/12/17.
+//  Copyright © 2017 Shreyas Aiyar. All rights reserved.
+//
+
+import UIKit
+
+class HomePage: UIViewController{
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+}
---
+++ Revels'18/Model/HomePageModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomePageModel.swift
+//  Revels-18
+//
+//  Created by Shreyas Aiyar on 08/12/17.
+//  Copyright © 2017 Shreyas Aiyar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeModel{
+    let homeItems:[String] = ["Test","Test","Test","Test","Test","Test","Test","Test"]
+}
