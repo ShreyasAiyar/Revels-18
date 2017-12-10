7e6d26f80be63f065a65a665d5d2a772b695cf65
ShreyasAiyar <shreyas.aiyar96@gmail.com>
2017-12-10 11:26:25 +0530

    Prior To Updation

---
+++ Revels'18/View/NewHomeViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewHomeViewCell.swift
+//  Revels-18
+//
+//  Created by Shreyas Aiyar on 09/12/17.
+//  Copyright © 2017 Shreyas Aiyar. All rights reserved.
+//
+
+import UIKit
+
+class NewHomeViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var homeLabel: UILabel!
+    
+}
